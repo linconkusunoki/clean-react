76ea41412429ecb1c89444ba41f6480fb25fc541
Lincon Kusunoki <linconkusunoki@gmail.com>
2021-07-15 17:00:38 +0200

    test: ensure AxiosHttpClient calls axios with correct verb

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -10,10 +10,10 @@ const makeSut = () => {
 }
 
 describe('AxiosHttpClient', () => {
-  test('should call axios with correct url', async () => {
+  test('should call axios with correct url and verb', async () => {
     const sut = makeSut()
     const url = faker.internet.url()
     await sut.post({ url })
-    expect(mockedAxios).toHaveBeenCalledWith(url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(url)
   })
 })
