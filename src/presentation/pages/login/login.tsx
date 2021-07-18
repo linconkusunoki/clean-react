2ba4cd8fbac24be7126d7140cf00ad3c1c0d68db
Lincon Kusunoki <linconkusunoki@gmail.com>
2021-07-18 20:17:51 +0200

    feat: ensure login starts with inputs with required state

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -5,11 +6,19 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 import { Header } from 'presentation/components'
-import styles from './login.styles.scss'
 
 export const Login = () => {
+  const [state] = useState({
+    isLoading: false
+  })
+  const [errorState] = useState({
+    main: '',
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório'
+  })
+
   return (
-    <div className={styles.login}>
+    <div>
       <Header />
       <Container>
         <Row>
@@ -17,19 +26,44 @@ export const Login = () => {
 
           <Col>
             <Form className="bg-white rounded-3 shadow-sm p-4">
-              <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Group className="mb-3" controlId="email">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Digite seu email" />
+                <Form.Control
+                  name="email"
+                  type="email"
+                  placeholder="Digite seu email"
+                  required
+                />
               </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Group className="mb-3" controlId="password">
                 <Form.Label>Senha</Form.Label>
-                <Form.Control type="password" placeholder="Digite sua senha" />
+                <Form.Control
+                  name="password"
+                  type="password"
+                  placeholder="Digite sua senha"
+                  required
+                />
               </Form.Group>
+
+              {errorState.main && <p className="text-danger">Error</p>}
 
               <div className="d-grid gap-2">
-                <Button variant="primary" type="submit">
-                  Entrar
+                <Button
+                  variant="primary"
+                  type="submit"
+                  disabled={state.isLoading}
+                >
+                  {state.isLoading && (
+                    <span
+                      className="spinner-grow spinner-grow-sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                  )}
+                  <span className={state.isLoading ? 'visually-hidden' : ''}>
+                    Entrar
+                  </span>
                 </Button>
 
                 <Button variant="secondary" type="button">
