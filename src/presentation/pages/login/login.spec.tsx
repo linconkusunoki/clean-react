921beaeb70d0d4ca6f7204c83aefd3a8a3e082d5
Lincon Kusunoki <linconkusunoki@gmail.com>
2021-07-20 17:36:45 +0200

    test: ensure Login navigates to main page on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -19,7 +19,7 @@ type SutParams = {
   validationError: string
 }
 
-const history = createMemoryHistory()
+const history = createMemoryHistory({ initialEntries: ['/login'] })
 
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub()
@@ -197,6 +197,9 @@ describe('Login component', () => {
           authenticationSpy.account.accessToken
         )
       })
+
+      expect(history.length).toBe(1)
+      expect(history.location.pathname).toBe('/')
     })
   })
 
