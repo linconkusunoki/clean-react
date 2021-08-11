13726983f4de6d54364261c7ca3857dba93217da
Lincon Kusunoki <linconkusunoki@gmail.com>
2021-08-11 17:51:53 +0200

    feat: ensure AxiosHttpAdapter calls axios.get with correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from 'axios'
 import {
+  HttpGetParams,
   HttpPostClient,
   HttpPostParams,
   HttpResponse
@@ -19,5 +20,9 @@ export class AxiosHttpAdapter implements HttpPostClient {
       statusCode: axiosResponse.status,
       body: axiosResponse.data
     }
+  }
+
+  async get(params: HttpGetParams): Promise<void> {
+    await axios.get(params.url)
   }
 }
