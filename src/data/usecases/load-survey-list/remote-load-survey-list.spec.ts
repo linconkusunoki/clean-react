d8ea7f0afe0b16ee254db8ba2f46be17b9ae4fe4
Lincon Kusunoki <linconkusunoki@gmail.com>
2021-08-11 17:29:34 +0200

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 404

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -1,14 +1,17 @@
 import faker from 'faker'
-import { HttpGetClientSpy } from 'data/test'
+import { HttpGetClientSpy, HttpPostClientSpy } from 'data/test'
 import { RemoteLoadSurveyList } from './remote-load-survey-list'
+import { HttpStatusCode } from 'data/protocols/http'
+import { UnexpectedError } from 'domain/errors'
+import { SurveyModel } from 'domain/models'
 
 type SutTypes = {
   sut: RemoteLoadSurveyList
-  httpGetClientSpy: HttpGetClientSpy
+  httpGetClientSpy: HttpGetClientSpy<SurveyModel[]>
 }
 
 const makeSut = (url = faker.internet.url()): SutTypes => {
-  const httpGetClientSpy = new HttpGetClientSpy()
+  const httpGetClientSpy = new HttpGetClientSpy<SurveyModel[]>()
   const sut = new RemoteLoadSurveyList(url, httpGetClientSpy)
 
   return {
@@ -23,5 +26,14 @@ describe('RemoteLoadSurveyList', () => {
     const { sut, httpGetClientSpy } = makeSut(url)
     sut.loadAll()
     expect(httpGetClientSpy.url).toBe(url)
+  })
+
+  test('should throw UnexpectedError if HttpGetClient return 403', () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+    const promise = sut.loadAll()
+    expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
